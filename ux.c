68d48cf810e1d3e99957c2055c3e7e3a997fc4c5
Igor Opaniuk <igor.opaniuk@oss.qualcomm.com>
2025-06-24 06:47:46 +0200

    ux: address checkpatch issues
    CHECK: Macro argument reuse 'x' - possible side-effects?
    +#define MIN(x, y) ((x) < (y) ? (x) : (y))
    
    CHECK: Macro argument reuse 'y' - possible side-effects?
    +#define MIN(x, y) ((x) < (y) ? (x) : (y))
    
    CHECK: line length of 167 exceeds 120 columns
    +static const char * const progress_hashes = "########################################################################################################################";
    
    CHECK: line length of 167 exceeds 120 columns
    +static const char * const progress_dashes = "------------------------------------------------------------------------------------------------------------------------";
    
    WARNING: please, no spaces at the start of a line
    +    CONSOLE_SCREEN_BUFFER_INFO csbi;$
    
    WARNING: please, no spaces at the start of a line
    +    HANDLE stdoutHandle = GetStdHandle(STD_OUTPUT_HANDLE);$
    
    WARNING: please, no spaces at the start of a line
    +    if (GetConsoleScreenBufferInfo(stdoutHandle, &csbi)) {$
    
    WARNING: suspect code indent for conditional statements (4, 16)
    +    if (GetConsoleScreenBufferInfo(stdoutHandle, &csbi)) {
    +		columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
    
    WARNING: please, no spaces at the start of a line
    +    }$
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@oss.qualcomm.com>

--- ux.c
+++ ux.c
@@ -9,13 +9,19 @@
 
 #include "qdl.h"
 
-#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define MIN(x, y) ({		\
+	__typeof__(x) _x = (x);	\
+	__typeof__(y) _y = (y);	\
+	_x < _y ? _x : _y;	\
+})
 
 #define UX_PROGRESS_REFRESH_RATE	10
 #define UX_PROGRESS_SIZE_MAX		120
 
-static const char * const progress_hashes = "########################################################################################################################";
-static const char * const progress_dashes = "------------------------------------------------------------------------------------------------------------------------";
+static const char * const progress_hashes = "############################################################"
+					    "############################################################";
+static const char * const progress_dashes = "------------------------------------------------------------"
+					    "-----------------------------------------------------------";
 
 static unsigned int ux_width;
 static unsigned int ux_cur_line_length;
@@ -44,15 +50,15 @@ static void ux_clear_line(void)
 
 void ux_init(void)
 {
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
 	int columns;
 
-    HANDLE stdoutHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	HANDLE stdoutHandle = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    if (GetConsoleScreenBufferInfo(stdoutHandle, &csbi)) {
+	if (GetConsoleScreenBufferInfo(stdoutHandle, &csbi)) {
 		columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
 		ux_width = MIN(columns, UX_PROGRESS_SIZE_MAX);
-    }
+	}
 }
 
 #else
@@ -160,9 +166,9 @@ void ux_progress(const char *fmt, unsigned int value, unsigned int max, ...)
 
 	printf("%-20.20s [%.*s%.*s] %1.2f%%%n\r", task_name,
 	       bars, progress_hashes,
-			dashes, progress_dashes,
-			percent * 100,
-			&ux_cur_line_length);
+	       dashes, progress_dashes,
+	       percent * 100,
+	       &ux_cur_line_length);
 	fflush(stdout);
 
 	gettimeofday(&last_progress_update, NULL);
